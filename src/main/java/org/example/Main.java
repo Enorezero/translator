5d42c714d83697a717af18612ab24eb6c6f981d0
Enorezero <98647618+Enorezero@users.noreply.github.com>
2024-12-08 21:50:58 +1000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,66 +15,22 @@ public class Main {
                 "    public static void main(String[] args) { " +
                 "        int a = 10; " +
                 "        int b = 3.14; " +
-                "        if (a < b) {a += b; } " +
+                "        if (a < b) {a + b; } " +
                 "    } " +
                 "} ";
 
         //String code = "int a = 5; int b = 4; if (a < b) { a+=b;}";
 
-        List<Token> tokens = new ArrayList<>();
 
-        // Массив токенов в виде пар (тип, значение)
-        String[][] tokenArray = {
-                {"PUBLIC", "public"},
-                {"CLASS", "class"},
-                {"IDENTIFIER", "HelloWorld"},
-                {"LBRACE", "{"},
-                {"PUBLIC", "public"},
-                {"STATIC", "static"},
-                {"VOID", "void"},
-                {"IDENTIFIER", "main"},
-                {"LPAREN", "("},
-                {"DATA_TYPE", "String[]"},
-                {"IDENTIFIER", "args"},
-                {"RPAREN", ")"},
-                {"LBRACE", "{"},
-                {"DATA_TYPE", "int"},
-                {"IDENTIFIER", "a"},
-                {"EQUALS", "="},
-                {"NUMBER", "10"},
-                {"SEMICOLON", ";"},
-                {"DATA_TYPE", "int"},
-                {"IDENTIFIER", "b"},
-                {"EQUALS", "="},
-                {"NUMBER", "3.14"},
-                {"SEMICOLON", ";"},
-                {"IF", "if"},
-                {"LPAREN", "("},
-                {"IDENTIFIER", "a"},
-                {"LESS_THAN", "<"},
-                {"IDENTIFIER", "b"},
-                {"RPAREN", ")"},
-                {"LBRACE", "{"},
-                {"IDENTIFIER", "a"},
-                {"PLUS", "+"},
-                {"IDENTIFIER", "b;"},
-                {"RBRACE", "}"},
-                {"RBRACE", "}"},
-        };
-
-        // Добавление токенов в список
-        for (String[] tokenData : tokenArray) {
-            TokenType type = TokenType.valueOf(tokenData[0]);
-            String value = tokenData[1];
-            tokens.add(new Token(type, value));
-        }
 
         Lexer lexer = new Lexer(code);
 
-        //System.out.println(tokens);
+        //System.out.println(lexer.tokenize());
 
-        Parser parser = new Parser(tokens);
-
+        Parser parser = new Parser(lexer.tokenize());
+//
+//        //System.out.println(lexer.tokenize());
+//
         List<ASTNode> nodes = parser.parse();
 
         for (ASTNode node : nodes) {
